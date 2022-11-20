77905919cf19aecf68419ee41d317be11b96ac32
Zsolt148 <b.zsolt148@gmail.com>
2022-11-20 13:28:15 +0100

    Gamf2 init

---
+++ Gamf2/Gamf2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Gamf2.Models;
+using Gamf2.Data;
+
+namespace Gamf2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly GAMFDbContext _context;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, GAMFDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Gamf2/Gamf2/Models/Enrollment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Gamf2.Models
+{
+    public enum Grade { A, B, C, D, F }
+
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+
+        [Display(Name = "Kurzus")]
+        public int CourseId { get; set; }
+
+        [Display(Name = "Hallgató")]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Jegy")]
+        public Grade? Grade { get; set; }
+
+        [Display(Name = "Kurzus")]
+        public virtual Course Course { get; set; }
+
+        [Display(Name = "Hallgató")]
+        public virtual Student Student { get; set; }
+
+        public Enrollment()
+        {
+        }
+    }
+}
+
