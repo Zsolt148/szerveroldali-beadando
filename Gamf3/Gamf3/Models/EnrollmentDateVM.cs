4d9859b21b21556276dda4dee27692db611f6964
Zsolt148 <b.zsolt148@gmail.com>
2022-11-20 14:04:35 +0100

    Gamf3 init

---
+++ Gamf3/Gamf3/Models/EnrollmentDateVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Gamf3.Models
+{
+	public class EnrollmentDateVM
+	{
+        public DateTimeOffset EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+
+        public EnrollmentDateVM()
+		{
+		}
+	}
+}
+
---
+++ Gamf3/Gamf3/Models/StudentsReportVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Gamf3.Models
+{
+	public class StudentsReportVM
+	{
+        public Student Student { get; set; }
+        public int CreditsSum { get; set; }
+
+		public StudentsReportVM()
+		{
+		}
+	}
+}
+
